c6ce1cf8077ec210ef832a46ddd5066cd9d92b7a
GregGamer <gregamer98@gmail.com>
2020-12-07 04:42:24 +0100

    improvements on day6 challenge

--- advent2020/p06b_form_analysis.py
+++ advent2020/p06b_form_analysis.py
@@ -1,13 +1,19 @@
 with open("advent2020/p06a_data.txt") as file :
     data = file.read().split("\n\n")
 
-erg = 0
+erg1 = 0
+erg2 = 0
 for d in data :
     group = d.split("\n")
-    intersect = set(group[0])
+    intersect1 = set(group[0])
+    intersect2 = set(group[0])
     for person in group[1:] :
-        print(len(person), set(person))
-        intersect = set(person).intersection(intersect)
-    print(len(intersect),intersect,"\n")
-    erg += len(intersect)
-print(erg)
+        # print(len(person), set(person))
+        intersect1 = set(person).intersection(intersect1)
+        intersect2 = set(person).union(intersect2)
+    # print(len(intersect),intersect,"\n")
+    erg1 += len(intersect1)
+    erg2 += len(intersect2)
+print("Ergebnis Beispiel 1:",erg2)
+print("Ergebnis Beispiel 2:",erg1)
+# print(erg1)
